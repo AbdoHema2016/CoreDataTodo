a6d93d10e05c19d3a45777c2d4d709d21a190c55
Abdelrahman-Arw <abdelrahman.arwish@linkdev.com>
2019-12-16 13:51:59 +0200

    Initial Commit

---
+++ CoreDataTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTodo
+//
+//  Created by Abdelrahman-Arw on 12/16/19.
+//  Copyright © 2019 Abdelrahman-Arw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
